a73e9f0144992977af2b32a9d5671992db6a8ace
Saulo Faria <saulo82faria@gmail.com>
2025-10-16 13:58:39 -0300

    Update leagues_overview.py
    Substack Link

--- leagues_overview.py
+++ leagues_overview.py
@@ -210,6 +210,7 @@ with st.sidebar:
     st.write("This Web App was designed in order to observe how teams are performing in their national leagues regarding xG Open Play Per 100 Passes (current season). If you want to have more details about these matches, other stats, other seasons or believe I can help you in your project, send an email to saulo.foot@gmail.com. I'm always open to work.")
 
     st.subheader("My links (pt-br)")
+    st.link_button("Substack", "https://substack.com/@saulofaria", use_container_width=True)
     st.link_button("Instagram", "https://www.instagram.com/saulo.foot/", use_container_width=True)
     st.link_button("X", "https://x.com/fariasaulo_", use_container_width=True)
     st.link_button("Youtube", "https://www.youtube.com/channel/UCkSw2eyetrr8TByFis0Uyug", use_container_width=True)
